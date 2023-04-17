f59c6d01058f7b11a87609f78548807ec2eda4ef
Isaiah Muli <i.dickson@carepay.com>
2023-04-17 20:40:52 +0300

    First exercise with streams

---
+++ README.md
@@ -0,0 +1,42 @@
+# Definition
+
+## Reactive Programming
+
+Reactive programming is a declarative programming paradigm concerned with data streams and the propagation of change.
+
+Declarative -> this is what I want to happen
+
+Imperative -> Specifying all the steps
+
+With this paradigm, it's possible to express static (e.g arrays) or dynamic
+(e.g event emitters) data streams with ease, and also communicate that an inferred dependency within the associated
+execution model exists
+
+### Traditional use cases
+
+1. User events
+    1. When user clicks a button -> reacting to a button click, do something when the button is clicked.
+    2. When I/O response happens.
+2. Something
+
+### Why do we need Reactive Programming?
+
+A mordern application development is characterized by
+
+1. High data scale
+2. High usage scale
+3. Cloud based costs
+
+##### Types of scaling
+
+1. Vertical scaling -> Increase processor capacity (e.g memory)
+2. Horizontal scaling-> adding more servers
+
+Before scaling, we need to optimize
+
+### Reactive Programming is:-
+
+- A different way of thinking about flow
+- A different way of thinking about data
+- Integrated with Java! 'Flow' interface
+
--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,5 +1,11 @@
 package io.javabrains.reactiveworkshop;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static io.javabrains.reactiveworkshop.StreamSources.intNumbersStream;
+import static io.javabrains.reactiveworkshop.StreamSources.userStream;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
@@ -7,24 +13,57 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        intNumbersStream().forEach(n -> printANumber(n));
+        printAString("***********");
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        intNumbersStream()
+                .filter(n -> n < 5)
+                .forEach(n -> printANumber(n));
+        printAString("***********");
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        intNumbersStream()
+                .filter(n -> n > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(n -> printANumber(n));
+        printAString("***********");
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        Integer value = intNumbersStream()
+                .filter(n -> n > 5)
+                .findFirst()
+                .orElse(-1);
+        printANumber(value);
+        printAString("***********");
 
         // Print first names of all users in userStream
         // TODO: Write code here
+        userStream().map(user -> user.getFirstName()).forEach(name -> printAString(name));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        Set<Integer> intNumbers = intNumbersStream().collect(Collectors.toSet());
+        userStream()
+                .filter(user -> intNumbers.contains(user.getId()))
+                .map(user -> user.getFirstName())
+                .forEach(Exercise1::printAString);
 
+        // OR
+        userStream()
+                .filter(user -> intNumbersStream().anyMatch(id -> id == user.getId()))
+                .map(user -> user.getFirstName())
+                .forEach(Exercise1::printAString);
+
+    }
+
+    private static void printANumber(Integer n) {
+        System.out.print(n);
+    }
+
+    private static void printAString(String n) {
+        System.out.println(n);
     }
 
 }
